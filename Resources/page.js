ed0db8f829b7eec1523e8ebbebb7126c9f9788c0
Sukriti Chatterjee <53958873+SukritiC@users.noreply.github.com>
2020-01-07 20:36:40 +0530

    Add files via upload

---
+++ Resources/page.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function pops(){
+    var img=document.getElementById("side");
+    img.style.display="block";
+}
+function off(){
+    var img=document.getElementById("side");
+    img.style.display="none";
+   
+}
